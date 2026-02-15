9375b6988cfad977d78ff8e7ed2c8bd1434fc652
Abhinav Srivastava <abhinav.srivastava1@cimpress.com>
2026-02-15 20:43:28 +0530

    trial constructors - yet not finalized

--- src/core/box.c
+++ src/core/box.c
@@ -1,2 +1,78 @@
 #include "headers/box.h"
 
+Box* boxConstructor(Vertex vertices[4], Color sideColors[4])
+{
+    Box *box = malloc(sizeof(Box));
+    
+    if(box == NULL)
+    {
+        return NULL;
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        if(!validateInputVertex(&vertices[i]))
+        {
+            free(box);
+            return NULL;
+        }
+
+        box->Vertices[i] = vertices[i];
+        box->SideColors[i] = sideColors[i];
+    }
+
+    return box;
+}
+
+ContainerBox* containerBoxConstructor(Vertex vertices[4], Color sideColors[4])
+{
+    Box *box = boxConstructor(vertices, sideColors);
+
+    if(box == NULL)
+    {
+        return NULL;
+    }
+
+    ContainerBox *containerBox = malloc(sizeof(ContainerBox));
+
+    if(containerBox == NULL)
+    {
+        free(box);
+        return NULL;
+    }
+
+    containerBox->box = box;
+    containerBox->childrenCount = 0;
+    return containerBox;
+}
+
+ListBox* listBoxConstructor(
+    Vertex vertices[4], Color sideColors[4], unsigned int childrenCount)
+{
+    Box *box = boxConstructor(vertices, sideColors);
+
+    if(box == NULL)
+    {
+        return NULL;
+    }
+
+    ListBox *listBox = malloc(sizeof(ListBox) + (childrenCount * sizeof(Box)));
+
+    if(listBox == NULL)
+    {
+        free(box);
+        return NULL;
+    }
+
+    listBox->box = box;
+    listBox->childrenCount = childrenCount;
+
+    for(int i = 0; i < childrenCount; i++)
+    {
+        listBox->children[i] = (Box){0};
+    }
+
+    return listBox;
+}
+void containerBoxDestructor(ContainerBox *containerBox);
+void listBoxDestructor(ListBox *containerBox);
--- src/core/headers/box.h
+++ src/core/headers/box.h
@@ -7,19 +7,23 @@ typedef struct _Box { Vertex Vertices[4]; Color SideColors[4]; } Box;
 typedef struct _ContainerBox ContainerBox;
 
 struct _ContainerBox { 
-    Box box;
+    Box *box;
     ContainerBox *children[MAX_CHILD_CONTAINERS];
     unsigned int childrenCount;
 };
 
 typedef struct _ListBox {
-    Box box;
+    Box *box;
     Box *children;
     unsigned int childrenCount;
 } ListBox;
 
-Box boxConstructor(Vertex vertices[4], Color sideColors[4]);
-ContainerBox* containerBoxConstructor(Box box);
-ListBox* listBoxConstructor(Box box, unsigned int childrenCount);
+Box* boxConstructor(Vertex vertices[4], Color sideColors[4]);
+ContainerBox* containerBoxConstructor(Vertex vertices[4], Color sideColors[4]);
+
+ListBox* listBoxConstructor(
+    Vertex vertices[4], Color sideColors[4], unsigned int childrenCount
+);
+
 void containerBoxDestructor(ContainerBox *containerBox);
 void listBoxDestructor(ListBox *containerBox);
